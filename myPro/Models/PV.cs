b0cf97e12da6e2a8dc978b7d3926a5de50dcaff2
Steven FUNG <fungkwokshun@icloud.com>
2017-03-20 02:24:12 +0800

    Create PV.cs

---
+++ myPro/Models/PV.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myPro.Models
+{
+	public class PV
+	{
+		public string Text { get; set; }
+	}
+
+	//public string Text="";
+}
