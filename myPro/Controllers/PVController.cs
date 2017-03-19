837844fb5507519b86fa38d02b2227a685c10b3b
Steven FUNG <fungkwokshun@icloud.com>
2017-03-20 00:08:34 +0800

    Update PVController.cs

--- myPro/Controllers/PVController.cs
+++ myPro/Controllers/PVController.cs
@@ -8,10 +8,18 @@ namespace myPro.Controllers
 {
     public class PVController : Controller
     {
-        public ActionResult PV()
-		{
-			ViewBag.part = "PartController
-			return PartialView();
-		}
+        public class MyModel
+	{
+		public int Prop1 { get; set; }
+		public int Prop2 { get; set; }
+	}
+
+        public PartialViewResult PV()
+	{
+		TempData["Hello"] = "Hello from PartController";
+		//return PartialView();
+		var data = new MyModel() { Prop1 = 5, Prop2 = 10 };
+		return PartialView("~/Views/Shared/PV.cshtml", data);
+	}
     }
 }
