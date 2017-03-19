5780efd98f44d37e309831525d8ea16b8a5cef8f
istevenfung <fungkwokshun@icloud.com>
2017-03-19 13:36:24 +0800

    Initial check-in of module WebMVC

---
+++ myPro/Controllers/ABController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myPro.Controllers
+{
+    public class ABController : Controller
+    {
+        public ActionResult PV()
+		{
+			ViewBag.part = "Part";
+			return PartialView();
+		}
+    }
+}
---
+++ myPro/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace myPro.Controllers
+{
+    public class TestController : Controller
+    {
+        public ActionResult Index()
+        {
+			ViewBag.test = "Testing...";
+			ViewBag.title = "Test Title";
+            return View();
+        }
+    }
+}
